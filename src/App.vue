e5c0bde695be713411ba05d7e15c1425ef11c075
Alexandr <sasha-is-permi@mail.ru>
2025-06-07 08:14:50 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+
+<script setup>
+const currentDate = new Date().toLocaleDateString()
+</script>
+
+
+<template>
+  <div>
+    Текущая дата: {{ currentDate }}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
